14dc0dfd6143c90c506c2e0ab4f9af6f89133b48
hayk <haykh.astro@gmail.com>
2023-12-21 05:52:44 -0500

    minor ui

--- ui/app/app.go
+++ ui/app/app.go
@@ -2,9 +2,15 @@ package app
 
 import "github.com/charmbracelet/lipgloss"
 
+var (
+	style = lipgloss.NewStyle().MarginTop(1)
+)
+
 func View(contents ...string) string {
-	return lipgloss.JoinVertical(
-		lipgloss.Left,
-		contents...,
+	return style.Render(
+		lipgloss.JoinVertical(
+			lipgloss.Left,
+			contents...,
+		),
 	)
 }
