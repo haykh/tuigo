853dd9a5ab07aeb7215bc8576a5522a165305e40
hayk <haykh.astro@gmail.com>
2023-12-21 04:38:52 -0500

    focusing bug

--- obj/container/container.go
+++ obj/container/container.go
@@ -130,11 +130,15 @@ func (c Container) Blur() obj.Collection {
 func (c Container) FocusNext() (obj.Collection, tea.Cmd) {
 	type FocusChangedMsg struct{}
 	if c.Focusable() && c.Focused() {
-		last_col_id := -1
-		for e, element := range c.elements {
-			if _, ok := element.(obj.Collection); ok {
-				if e > last_col_id {
-					last_col_id = e
+		last_foc_id := -1
+		for e := len(c.elements) - 1; e >= 0; e-- {
+			element := c.elements[e]
+			if col, ok := element.(obj.Collection); ok {
+				if col.Focusable() {
+					if e > last_foc_id {
+						last_foc_id = e
+						break
+					}
 				}
 			}
 		}
@@ -152,7 +156,7 @@ func (c Container) FocusNext() (obj.Collection, tea.Cmd) {
 					if cmd != nil {
 						switch cmd().(type) {
 						case utils.FocusNextMsg:
-							if e < last_col_id {
+							if e < last_foc_id {
 								c.elements[e] = el.Blur()
 							}
 							focus_next = true
@@ -173,11 +177,14 @@ func (c Container) FocusNext() (obj.Collection, tea.Cmd) {
 func (c Container) FocusPrev() (obj.Collection, tea.Cmd) {
 	type FocusChangedMsg struct{}
 	if c.Focusable() && c.Focused() {
-		first_col_id := len(c.elements) + 1
+		first_foc_id := len(c.elements) + 1
 		for e, element := range c.elements {
-			if _, ok := element.(obj.Collection); ok {
-				if e < first_col_id {
-					first_col_id = e
+			if col, ok := element.(obj.Collection); ok {
+				if e < first_foc_id {
+					if col.Focusable() {
+						first_foc_id = e
+						break
+					}
 				}
 			}
 		}
@@ -196,7 +203,7 @@ func (c Container) FocusPrev() (obj.Collection, tea.Cmd) {
 					if cmd != nil {
 						switch cmd().(type) {
 						case utils.FocusPrevMsg:
-							if e > first_col_id+1 {
+							if e > first_foc_id {
 								c.elements[e] = el.Blur()
 							}
 							focus_prev = true
